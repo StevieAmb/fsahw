c7e7e9b69d93270e20d88348e753c18ccf233d77
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-03 09:58:24 -0700

    Fix syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,8 @@ const App = () => {
   const courseParts = [
     {part1: 'Fundamentals of React', exercises1: 10}, 
     {part2: 'Using props to pass data', exercises2: 7}, 
-    {part3: 'State of a component', exercises3: 14} ]
+    {part3: 'State of a component', exercises3: 14} 
+  ]
 
   return (
     <div>

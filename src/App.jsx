e1101601d7c9847912b2238ba48d0fb5f65a2e45
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-16 13:26:21 -0700

    Remove unicafe folder and create components for assignments

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,44 @@
 
-const Header = (props) => {
+import { useState } from 'react'
+
+const Header = ({ text }) => {
   return (
-    <h1>{props.course}</h1>
+    <header>{text}</header>
   )
 }
 
-const Content = (props) => {
-  console.log('content', props.courses[0].part1)
+const Button = ({handleClick, text}) => {
   return (
-    <>
-      <Part course={props.courses[0].part1} exercise={props.courses[0].exercises1} />
-      <Part course={props.courses[1].part2} exercise={props.courses[1].exercises2} />
-      <Part course={props.courses[2].part3} exercise={props.courses[2].exercises3} />
-    </>
+    <button onClick={handleClick}>{text}</button>
   )
 }
-
-const Part = (props) => {
-  return (
-    <p>{props.course} {props.exercise}</p>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
-  const courseParts = [
-    {part1: 'Fundamentals of React', exercises1: 10}, 
-    {part2: 'Using props to pass data', exercises2: 7}, 
-    {part3: 'State of a component', exercises3: 14} 
-  ]
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodReviewClicks = () => {
+    let updatedGoodReviews = good + 1
+    setGood(updatedGoodReviews)
+  }
+
+  const handleBadReviewClicks = () => {
+    let updatedGoodReviews = good + 1
+    setGood(updatedGoodReviews)
+  }
+
+  const handleNeutralReviewClicks = () => {
+    let updatedGoodReviews = good + 1
+    setGood(updatedGoodReviews)
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content courses={courseParts}/>
-      <Total total={exercises1+exercises2+exercises3} />
+      <Header
+      text='Give Feedback'
+      />
     </div>
   )
 }

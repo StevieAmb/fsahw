2ada2ed5bf0c7dee69f97f0caccb3a98c376f408
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-17 14:15:07 -0700

    Start new homework assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,21 @@
 
-import { useState } from 'react'
-
-const Header = ({ text }) => {
-  return (
-    <h1>{text}</h1>
-  )
-}
-
-const Button = ({handleClick, text}) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
-}
-
-//conditionally render at top level the UI of the state when no feedback is given,
-//and pull it out of the statistics component.
-
-const StatisticLine = ({text, value}) => {
-  return (
-    <tr>
-      <td>
-        {text}:
-      </td>
-      <td>
-        {value}
-      </td>
-    </tr>
-  )
-
-}
-
-const Statistics = ({goodReviews, badReviews, neutralReviews}) => {
-  return (
-      <table>
-        <tbody>
-          <StatisticLine text='good' value={goodReviews} />
-          <StatisticLine text='neutral' value={neutralReviews} />
-          <StatisticLine text='bad' value={badReviews} />
-          <StatisticLine text='all' value={goodReviews + neutralReviews + badReviews} />
-          <StatisticLine text='positive' value={goodReviews / (goodReviews + neutralReviews + badReviews)} />
-        </tbody>
-      </table>
-  )
-}
-
 const App = () => {
-
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const handleGoodReviewClicks = () => {
-    let updatedGoodReviews = good + 1
-    setGood(updatedGoodReviews)
-  }
-
-  const handleBadReviewClicks = () => {
-    let updatedBadReviews = bad + 1
-    setBad(updatedBadReviews)
-  }
-
-  const handleNeutralReviewClicks = () => {
-    let updatedNeutralReviews = neutral + 1
-    setNeutral(updatedNeutralReviews)
-  }
-
-
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <Header
-      text='Give Feedback'
-      />
-      <Button handleClick={handleGoodReviewClicks} text='good' />
-      <Button handleClick={handleNeutralReviewClicks} text='neutral' />
-      <Button handleClick={handleBadReviewClicks} text='bad' />
-      <Header
-      text='Statistics'
-      />
-      {!good && !bad && !neutral ? <p>No feedback given</p> : <Statistics goodReviews={good} badReviews={bad} neutralReviews={neutral} />}
+      {anecdotes[selected]}
     </div>
   )
 }

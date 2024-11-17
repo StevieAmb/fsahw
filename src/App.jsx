5573e841c570c52bbae16957e1f53e6950fbb3b4
Stevie Ambroise <s.ambroise@protonmail.com>
2024-11-17 10:09:20 -0700

    Add all parts of statistics line

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ const App = () => {
         <StatisticLine text='neutral' value={neutral} />
         <StatisticLine text='bad' value={bad} />
         <StatisticLine text='all' value={good + neutral + bad} />
-        <StatisticLine text='positive' value={good + neutral + bad} />
+        <StatisticLine text='positive' value={good / (good + neutral + bad)} />
 
       </section>
     </div>

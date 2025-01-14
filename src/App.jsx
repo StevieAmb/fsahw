a0b2f0c5b3e76f08a36629f6b3fed524a9050e68
Stevie Ambroise <s.ambroise@protonmail.com>
2025-01-13 19:46:08 -0700

    Add classname to list

--- src/App.jsx
+++ src/App.jsx
@@ -276,7 +276,7 @@ const List = ({id, person, number, deleteNameAndNumber}) => {
   console.log(id)
   return (
     <ul>
-      <li>{person} {number} <button onClick={() => deleteNameAndNumber(id)}>delete</button></li> 
+      <li className="note">{person} {number} <button onClick={() => deleteNameAndNumber(id)}>delete</button></li> 
     </ul>
   )
 }
